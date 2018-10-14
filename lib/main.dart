b5b5c4c98a43cc0cdac2bdc2af35b55e00e9e52a
Heritage Samuel F <herrytheeagle@gmail.com>
2018-10-14 17:52:13 +0100

    imprvd

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,11 @@
 import 'package:flutter/material.dart';
 
+import './product_manager.dart';
+import './products.dart';
+
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-    State<StatefulWidget> createState() {
-      // TODO: implement createState
-      return _MyAppState();
-    }
-}
-
-class _MyAppState extends State<MyApp> {
-  List<String> _products = ['Eaglet tester'];
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,35 +13,8 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
          title: Text('EagleApp'),
         ),
-        body: Column(
-          children: [
-            Container(
-              margin: EdgeInsets.all(10.0),
-              child: RaisedButton(
-                onPressed: () {
-                  setState(() {
-                    _products.add('Advanced Eaglet Tester');
-                                    });
-                },
-                child: Text('Add Nest'),
-              ),
-            ),
-            Column(
-              children: _products
-                .map(
-                  (element) => Card(
-                        child: Column(
-                          children: <Widget>[
-                            Image.asset('assets/food.jpg'),
-                            Text(element)
-                          ],
-                        ),
-                      ),
-                    ) 
-                    .toList()
-            ),
-          ],
-        ),),
+        body: ProductManager(),
+        ),
       );
     }
   }
